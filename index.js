39ba3bb3829c9de039583072adec4b9cdbab16d2
Antti Manninen <antti.manninen@voidmainvoid.fi>
2020-04-09 13:32:02 +0300

    Allow unused variables that starts with _. When implementing interface that requires to return promises, it is sometimes useful return also non-promise. Disable require-await check.

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     'array-callback-return': 'error',
     'prefer-object-spread': 'error',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // allow variables that starts with _
     'import/no-unresolved': 0, // Doesn't work with TypeScript modules, see https://github.com/benmosher/eslint-plugin-import/issues/1120
     'mocha/no-mocha-arrows': 0,
     'mocha/no-setup-in-describe': 0, // This would be nice, but currently it breaks too much existing code.
@@ -55,7 +56,9 @@ module.exports = {
         '@typescript-eslint/no-non-null-assertion': 0, // We use these quite extensively when the compiler isn't smart enough.
         '@typescript-eslint/no-use-before-define': 0,
         '@typescript-eslint/no-var-requires': 0,
-        '@typescript-eslint/unbound-method': 0 // We like using libraries like lodash or Ramda, where using "methods" this way is the norm.
+        '@typescript-eslint/unbound-method': 0, // We like using libraries like lodash or Ramda, where using "methods" this way is the norm.
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // allow variables that starts with _
+        '@typescript-eslint/require-await': 0 // allow functions that returns non-promise also. Useful when implementing interfaces with possible promises
       }
     }
   ]
