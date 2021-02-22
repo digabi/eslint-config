057ff289b0bef646082f182cd9dc80de127aa622
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2021-02-22 22:09:06 +0200

    Simplify config
    @typescript-eslint/recommended-requiring-typechecking implies
    @typescript-eslint/eslint-recommended and
    @typescript-eslint/recommended, so we don't have add all three.

--- index.js
+++ index.js
@@ -67,11 +67,7 @@ module.exports = {
   overrides: [
     {
       files: ['**.ts', '**.tsx'],
-      extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
+      extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
         project: './tsconfig.json',
